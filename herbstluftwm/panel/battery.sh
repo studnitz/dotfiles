29fb3c3e4aaab7e1aba9e556a25613418328cc10
Alexander von Studnitz <alexander@studnitz.net>
2016-02-09 18:54:00 +0100

    Fix battery.sh not working on percentages between 100 and 36

--- herbstluftwm/panel/battery.sh
+++ herbstluftwm/panel/battery.sh
@@ -5,15 +5,16 @@ battery=$(upower -e | grep BAT0)
 bat_percent=$(upower -i $battery | grep 'percentage' | grep -o '[0-9]*')
 bat_state=$(upower -i $battery | grep 'state' | awk '{print $2}')
 battery_icon="^fn($icons)^fn()"
+width=$((xftwidth "$icons" "")) # Calculate offset of icon
 
 if [[ "$bat_state" = "discharging" ]]
 then
     if (("$bat_percent" == 100))
     then
-        battery_icon="^fg(#C9FF1A)^fn($icons)^fn()^fg"
+        battery_icon="^fg(#C9FF1A)^fn($icons)^fn()"
     elif (("$bat_percent" < 100)) && (("$bat_percent" >= 70))
     then
-        battery_icon="^fg(#C9FF1A)^fn($icons)^fn()^fg"
+        battery_icon="^fg(#C9FF1A)^fn($icons)^fn()"
     elif (("$bat_percent" < 70)) && (("$bat_percent" >= 36))
     then
         battery_icon="^fg(#F6FF1A)^fn($icons)^fn()"
@@ -25,6 +26,5 @@ else #Charging
     battery_icon="^fg(#C9FF1A)^fn($icons)^fn()"
 fi
 
-width=$((xftwidth "$icons" "")) # Calculate offset of icon
 
 echo "$battery_icon^p($width)^fg() $bat_percent%"
